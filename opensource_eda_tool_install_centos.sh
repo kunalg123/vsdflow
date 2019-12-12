5abc7295c030682d5fa8fd742846df8adc31de40
Kunal Ghosh <kunalpghosh@gmail.com>
2019-12-12 19:16:15 +0530

    Update opensource_eda_tool_install_centos.sh

--- opensource_eda_tool_install_centos.sh
+++ opensource_eda_tool_install_centos.sh
@@ -1,8 +1,8 @@
 mkdir -p work/tools
-mv qrouter-1.4.31.tgz work/tools/.
-mv magic-8.1.220.tgz work/tools/.
-mv netgen-1.5.100.tgz work/tools/.
-mv qflow-1.2.89.tgz work/tools/.
+mv qrouter-1.4.59.tgz work/tools/.
+mv magic-8.2.172.tgz work/tools/.
+mv netgen-1.5.134.tgz work/tools/.
+mv qflow-1.3.17.tgz work/tools/.
 cd work/tools
 sudo yum install  autoconf automake binutils  bison flex gcc gcc-c++ gettext libtool make patch pkgconfig redhat-rpm-config rpm-build rpm-sign ctags elfutils indent patchutils -y
 wget "https://github.com/Kitware/CMake/releases/download/v3.13.0/cmake-3.13.0.tar.gz"
@@ -51,23 +51,23 @@ cd ../../
 yum install  xorg-x11-server-Xorg xorg-x11-xauth xorg-x11-apps -y
 sudo yum install libXt-devel -y
 
-tar -xvzf qrouter-1.4.31.tgz
-cd qrouter-1.4.31
+tar -xvzf qrouter-1.4.59.tgz
+cd qrouter-1.4.59
 sudo ./configure
 sudo make
 sudo make install
 cd ../
 
 sudo yum install libX11-devel -y
-tar -xvzf magic-8.1.220.tgz
-cd magic-8.1.220
+tar -xvzf magic-8.2.172.tgz
+cd magic-8.2.172
 sudo ./configure
 sudo make
 sudo make install
 cd ../
 
-tar -xvzf netgen-1.5.100.tgz
-cd netgen-1.5.100
+tar -xvzf netgen-1.5.134.tgz
+cd netgen-1.5.134
 sudo ./configure
 sudo make
 sudo make install
@@ -78,8 +78,8 @@ sudo ln -s /usr/local/bin/qrouter /usr/bin/qrouter
 sudo ln -s /usr/local/bin/magic /usr/bin/magic
 sudo ln -s /usr/local/bin/netgen /usr/bin/netgen
 sudo ln -s /usr/local/bin/yosys-abc /usr/bin/yosys-abc
-tar -xvzf qflow-1.2.89.tgz
-cd qflow-1.2.89
+tar -xvzf qflow-1.3.17.tgz
+cd qflow-1.3.17
 sudo ./configure
 sudo make
 sudo make install
